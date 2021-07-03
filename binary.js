c02abc365b09652c4a418f1cbea88108656457c7
sammykirigha <dkirigha18@gmail.com>
2021-07-03 13:17:15 +0300

    added a second method

--- binary.js
+++ binary.js
@@ -14,3 +14,25 @@ function binarySearch(arr, elem) {
 }
 
 console.log(binarySearch([1,2,4,5,6,7,12,13,15,19,20,25,30,39,45,60], 45))
+
+//METHOD 2
+
+function binary(array, value) {
+    let guess,
+        min = 0,
+        max = array.length - 1;
+    while (min <= max) {
+        guess = Math.floor((min + max) / 2)
+        if (value === array[guess]) {
+            return guess
+        } else if (value > array[guess]) {
+            min = guess + 1;
+        } else {
+            max = guess - 1;
+        }
+    }
+    console.log(guess)
+    return -1;
+}
+
+console.log(binary([1, 2, 4, 5, 6, 7, 12, 13, 15, 19, 20, 25, 30, 39, 45, 60], 450))
